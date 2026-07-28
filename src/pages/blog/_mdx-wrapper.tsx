5e76a9df374181d66c58d5ea6011c17f60377997
Derek Cofausper <256792747+decofe@users.noreply.github.com>
2026-07-28 19:42:59 +0000

    revert: restore intentional blog layout

--- src/pages/blog/_mdx-wrapper.tsx
+++ src/pages/blog/_mdx-wrapper.tsx
@@ -1,11 +1,6 @@
 import type { ReactNode } from "react";
-import { Layout } from "vocs";
 import { BlogPostLayout } from "../../components/BlogPostLayout.js";
 
 export default function BlogWrapper({ children }: { children: ReactNode }) {
-  return (
-    <Layout>
-      <BlogPostLayout>{children}</BlogPostLayout>
-    </Layout>
-  );
+  return <BlogPostLayout>{children}</BlogPostLayout>;
 }
